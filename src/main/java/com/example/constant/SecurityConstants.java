1af2f9b0b58ce2570db7b1bfaf4a1b30449b842d
云逸 <52526354+vains-Sofia@users.noreply.github.com>
2023-08-05 09:25:27 +0800

    Update SecurityConstants.java

--- src/main/java/com/example/constant/SecurityConstants.java
+++ src/main/java/com/example/constant/SecurityConstants.java
@@ -10,17 +10,17 @@ public class SecurityConstants {
     /**
      * 授权确认页面地址
      */
-    public static final String DEVICE_ACTIVATED_URI = "http://127.0.0.1:5173/activated";
+    public static final String DEVICE_ACTIVATED_URI = "/activated";
 
     /**
      * 授权确认页面地址
      */
-    public static final String DEVICE_ACTIVATE_URI = "http://127.0.0.1:5173/activate";
+    public static final String DEVICE_ACTIVATE_URI = "/activate";
 
     /**
      * 授权确认页面地址
      */
-    public static final String CONSENT_PAGE_URI = "http://127.0.0.1:5173/consent";
+    public static final String CONSENT_PAGE_URI = "/oauth2/consent";
 
     /**
      * 微信登录相关参数——openid：用户唯一id
